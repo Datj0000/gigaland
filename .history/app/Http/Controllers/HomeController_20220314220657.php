904d46aacd86f301f2be257074c0c0c30ca36690
ạt nguyễn <you@example.comdn16092000@gmail.com>
2022-03-14 22:41:29 +0700

    v3

---
+++ .history/app/Http/Controllers/HomeController_20220314220657.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Redirect;
+use App\Models\Customer;
+use App\Models\BuyPackage;
+use Carbon\Carbon;
+class HomeController extends Controller
+{
+    public function view_home()
+    {
+        $customer_id = Session::get('customer_id');
+        $buypackage_id = 0;
+        if ($customer_id) {
+            $today = Carbon::now('Asia/Ho_Chi_Minh');
+            $check_package = BuyPackage::where('customer_id',$customer_id)->where('status',0)->where('created_at','<=',$today)->first();
+            $check_hour = Carbon::now('Asia/Ho_Chi_Minh')->format('H-i');
+            if ($check_package && '06-00' <= $check_hour && $check_hour <= '06-30') {
+                $buypackage_id = $check_package->buypackage_id;
+            }
+            $customer = Customer::where('customer_id',$customer_id)->first()
+            return view('user.home')->with('buypackage_id', $buypackage_id);
+        }
+        else{
+            return view('user.home')->with('buypackage_id', $buypackage_id);
+        }
+    }
+    public function view_login()
+    {
+        $customer_id = Session::get('customer_id');
+        if ($customer_id) {
+            return Redirect::to('/');
+        }
+        else{
+            return view('user.login');
+        }
+    }
+    public function view_register()
+    {
+        $customer_id = Session::get('customer_id');
+        if ($customer_id) {
+            return Redirect::to('/');
+        }
+        else{
+            return view('user.register');
+        }
+    }
+    public function view_profile()
+    {
+        $customer_id = Session::get('customer_id');
+        if ($customer_id) {
+            return view('user.profile');
+        }
+        else{
+            return Redirect::to('/dang-nhap');
+        }
+    }
+    public function view_changepass()
+    {
+        $customer_id = Session::get('customer_id');
+        $customer_pass = Session::get('customer_pass');
+        if(!$customer_pass){
+            return Redirect::to('/');
+        }
+        else if ($customer_id) {
+            return view('user.changepass');
+        }
+        else{
+            return Redirect::to('/dang-nhap');
+        }
+    }
+    public function view_buy()
+    {
+        $customer_id = Session::get('customer_id');
+        if ($customer_id) {
+            return view('user.buy');
+        }
+        else{
+            return Redirect::to('/dang-nhap');
+        }
+    }
+    public function view_recharge()
+    {
+        $customer_id = Session::get('customer_id');
+        if ($customer_id) {
+            return view('user.recharge');
+        }
+        else{
+            return Redirect::to('/dang-nhap');
+        }
+    }
+    public function test(Request $request)
+    {
+        return view('test');
+    }
+}
---
+++ .history/resources/views/user/recharge.blade_20220314214112.php
@@ -0,0 +1,294 @@
+@extends('userlayout')
+@section('content')
+<!-- header begin -->
+<header class="transparent">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="de-flex sm-pt10">
+                    <div class="de-flex-col">
+                        <div class="de-flex-col">
+                            <!-- logo begin -->
+                            <div id="logo">
+                                <a href="{{URL::to('/')}}">
+                                    <img alt="" src="{{ asset('frontend/images/logo-1.png')}}" />
+                                </a>
+                            </div>
+                            <!-- logo close -->
+                        </div>
+                    </div>
+                    <div class="de-flex-col header-col-mid customer">
+                        <!-- mainmenu begin -->
+                        <div class="menu_side_area">
+                            <?php
+                                $customer_id = Session::get('customer_id');
+                                $customer_name = Session::get('customer_name');
+                                $customer_image = Session::get('customer_image');
+                                $customer_type = Session::get('customer_type');
+                                if(isset($customer_id)){
+                            ?>
+                            <div class="de-login-menu">
+                                <a href="create-options.html" class="btn-main"><i
+                                        class="fa fa-plus"></i><span>Create</span></a>
+
+                                <span id="de-click-menu-notification" class="de-menu-notification">
+                                    <span class="d-count">8</span>
+                                    <i class="fa fa-bell"></i>
+                                </span>
+
+                                <span id="de-click-menu-profile" class="de-menu-profile">
+                                    <?php
+                                            $customer_image = Session::get('customer_image');
+                                            if(isset($customer_image) && isset($customer_type)){
+                                        ?>
+                                    <img src="{{ $customer_image}}" class="img-fluid" alt="">
+                                    <?php
+                                        }else if(isset($customer_image)){
+                                        ?>
+                                    <img src="{{ asset('uploads/avatar/' . $customer_image . '')}}" class="img-fluid"
+                                        alt="">
+                                    <?php
+                                        }else{
+                                        ?>
+                                    <img src="{{ asset('backend/media/users/blank.png')}}" class="img-fluid" alt="">
+                                    <?php
+                                        }
+                                        ?>
+                                </span>
+
+                                <div id="de-submenu-notification" class="de-submenu">
+                                    <div class="de-flex">
+                                        <div>
+                                            <h4>Notifications</h4>
+                                        </div>
+                                        <a href="#">Show all</a>
+                                    </div>
+
+                                    <ul>
+                                        <li>
+                                            <a href="#">
+                                                <img class="lazy"
+                                                    src="{{ asset('frontend/images/author/author-2.jpg')}}" alt="">
+                                                <div class="d-desc">
+                                                    <span class="d-name"><b>Mamie Barnett</b> started following
+                                                        you</span>
+                                                    <span class="d-time">1 hour ago</span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img class="lazy"
+                                                    src="{{ asset('frontend/images/author/author-3.jpg')}}" alt="">
+                                                <div class="d-desc">
+                                                    <span class="d-name"><b>Nicholas Daniels</b> liked your item</span>
+                                                    <span class="d-time">2 hours ago</span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img class="lazy"
+                                                    src="{{ asset('frontend/images/author/author-4.jpg')}}" alt="">
+                                                <div class="d-desc">
+                                                    <span class="d-name"><b>Lori Hart</b> started following you</span>
+                                                    <span class="d-time">18 hours ago</span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img class="lazy"
+                                                    src="{{ asset('frontend/images/author/author-5.jpg')}}" alt="">
+                                                <div class="d-desc">
+                                                    <span class="d-name"><b>Jimmy Wright</b> liked your item</span>
+                                                    <span class="d-time">1 day ago</span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img class="lazy"
+                                                    src="{{ asset('frontend/images/author/author-6.jpg')}}" alt="">
+                                                <div class="d-desc">
+                                                    <span class="d-name"><b>Karla Sharp</b> started following you</span>
+                                                    <span class="d-time">3 days ago</span>
+                                                </div>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div id="de-submenu-profile" class="de-submenu">
+                                    <div class="d-name">
+                                        <h4>{{$customer_name}}</h4>
+                                        <a href="profile.html">Set display name</a>
+                                    </div>
+                                    <div class="spacer-10"></div>
+                                    <div class="d-wallet">
+                                        <h4>Balance</h4>
+                                        <span id="wallet"class="d-wallet-address">12.858 ETH</span>
+                                        <button id="btn_copy"><a href="{{ URL::to('/recharge') }}">recharge</a></button>
+                                    </div>
+                                    <div class="d-line"></div>
+                                    <ul class="de-submenu-profile">
+                                        <li><a href="{{ URL::to('/profile') }}"><i class="fa fa-user"></i> Edit profile</a>
+                                            <?php
+                                        $check_pass = Session::get('customer_pass');
+                                        if($check_pass){
+                                        ?>
+                                        <li><a href="{{ URL::to('/changepass') }}"><i class="fa fa-pencil"></i> Change
+                                                your password</a>
+                                            <?php
+                                        }
+                                        ?>
+                                        <li><a href="#" id="logout"><i class="fa fa-sign-out"></i> Sign out</a>
+                                    </ul>
+                                </div>
+                            </div>
+                            <?php
+                            }
+                            else {
+                                ?>
+                            <a href="{{ URL::to('/login') }}" class="btn-main btn-wallet"><i
+                                    class="icon_wallet_alt"></i><span>Login</span></a>
+                            <a href="{{ URL::to('/register') }}" class="btn-main btn-wallet"><i
+                                    class="icon_wallet_alt"></i><span>Register</span></a>
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="no-bottom no-top" id="content">
+    <div id="top"></div>
+    <section id="subheader" class="text-light" data-bgimage="url(frontend/images/background/subheader.jpg) top">
+        <div class="center-y relative text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h1>Recharge</h1>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="section-main" aria-label="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <form id="form-create-item" class="form-border needs-validation">
+                        <div class="de_tab tab_simple">
+                            <div class="de_tab_content">
+                                <div class="tab-1">
+                                    <div class="row wow fadeIn">
+                                        <div class="col-lg-8 mb-sm-20">
+                                            <div class="field-set">
+                                                <h5>Amount</h5>
+                                                <input type="text" name="amount" id="inp_amount" class="form-control"
+                                                    placeholder="Enter Amount Here" />
+                                                <div class="spacer-10"></div>
+                                                <div class="invalid-feedback">Please check amount</div>
+                                                <div class="spacer-10"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="spacer-30"></div>
+                        <input type="submit" id="submit" class="btn-main" value="Update profile">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<script type="text/javascript">
+    document.querySelector('#old-password').addEventListener('blur', validateOldpassword);
+    document.querySelector('#new-password').addEventListener('blur', validateNewpassword);
+    document.querySelector('#renew-password').addEventListener('blur', validateRepassword);
+    const reSpaces = /^\S*$/;
+
+    function validateOldpassword(e) {
+        const password = document.querySelector('#old-password');
+        if (password.value != "") {
+            password.classList.remove('is-invalid');
+            password.classList.add('is-valid');
+            return true;
+        } else {
+            password.classList.remove('is-valid');
+            password.classList.add('is-invalid');
+            return false;
+        }
+    }
+    function validateNewpassword() {
+        const password = document.querySelector('#new-password');
+        const re = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})(?=.*[!@#$%^&*])/;
+        if (re.test(password.value) && reSpaces.test(password.value)) {
+            password.classList.remove('is-invalid');
+            password.classList.add('is-valid');
+            return true;
+        } else {
+            password.classList.add('is-invalid');
+            password.classList.remove('is-valid');
+            return false;
+        }
+    }
+    function validateRePassword() {
+        const password = document.querySelector('#new-password');
+        const re_password = document.querySelector('#re-password');
+        if (password.value == re_password.value) {
+            re_password.classList.remove('is-invalid');
+            re_password.classList.add('is-valid');
+            return true;
+        } else {
+            re_password.classList.add('is-invalid');
+            re_password.classList.remove('is-valid');
+            return false;
+        }
+    }
+    (function () {
+    const forms = document.querySelectorAll('.needs-validation');
+    for (let form of forms) {
+        form.addEventListener(
+        'submit',
+        function (event) {
+            if (
+            !form.checkValidity() ||
+            !validateEmail() ||
+            !validateName() ||
+            !validatePhone()
+            ) {
+                event.preventDefault();
+                event.stopPropagation();
+                Swal.fire("","Please double check the fields","warning");
+            } else {
+                event.preventDefault();
+                axios.post("change-password-user", {
+                    old_pass: $('#old-password').val(),
+                    new_pass: $('#new-password').val(),
+                })
+                .then(function (response) {
+                    switch(response.data) {
+                        case 0:
+                            Swal.fire('','Old password is not correct','warning')
+                            break;
+                        case 1:
+                            Swal.fire('','Change password successfully','warning')
+                            break;
+                    }
+                })
+            }
+        },
+        false
+        );
+    }
+    })();
+</script>
+@endsection
